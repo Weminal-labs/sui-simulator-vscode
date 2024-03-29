0ca9018aa3e3009a68ac6503275f34cf7e248d12
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-03-29 12:45:09 +0700

    refactor: manage state into context

--- src/webview/features/suiConfig/v2/index.tsx
+++ src/webview/features/suiConfig/v2/index.tsx
@@ -4,22 +4,22 @@ import "react-toggle/style.css";
 import { convertWindowsToUnixPath } from "../../../utils";
 import { FileWithPath } from "../../../../types";
 import { useSuiClientContext } from "@mysten/dapp-kit";
-import Collapsible from 'react-collapsible';
+import Collapsible from "react-collapsible";
 import { messageHandler } from "@estruyf/vscode/dist/client";
+import { useSuiConfig } from "../../../context/SuiConfigProvider";
 
 export interface TerminalResponse {
   stdout: string;
   stderr: string;
-};
+}
 
 const requestData = async (action: string) => {
   return messageHandler.request<TerminalResponse>(action);
 };
 
 export const SuiConfig = () => {
-  const [isSuiCargo, setIsSuiCargo] = useState<boolean>(false);
-  const [suiPath, setSuiPath] = useState<string>("");
   const { network, selectNetwork } = useSuiClientContext();
+  const { isSuiCargo, setIsSuiCargo, suiPath, setSuiPath } = useSuiConfig();
 
   const handleNetworkChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     selectNetwork(e.target.value);
