d70e34a0751450205f59ee30b57785cee1fddf98
Le Khac Thanh Tung <lekhacthanhtung.it@gmail.com>
2024-04-05 14:19:43 +0700

    chore: remove unused code

--- src/webview/App.tsx
+++ src/webview/App.tsx
@@ -1,8 +1,6 @@
 import React, { useReducer, useState } from "react";
 import "./style.css";
 import { useSuiClient, useSuiClientContext } from "@mysten/dapp-kit";
-import { Input } from "./components/Input";
-import { Button } from "./components/Button";
 import { Aliases } from './components/Aliases';
 import { ActionType, MoveCallState } from "../types";
 import { MoveCallActionType, MoveCallStatus } from "../enums";
@@ -15,150 +13,142 @@ import { BuildTestPublish } from "./features/buildTestPublish";
 import { MySuiAccountProvider } from "./context/MySuiAccountProvider";
 
 const initialState: MoveCallState = {
-    mnemonics: "mouse hood crucial soup report axis awful point stairs guess scrap winter",
-    status: MoveCallStatus.BEGIN,
-    packageId: "",
-    modules: [],
-    currentModule: "",
-    functions: {},
-    currentFunction: "",
-    args: [],
-    argsUserInput: [],
-    error: "",
-    response: "",
+  mnemonics: "mouse hood crucial soup report axis awful point stairs guess scrap winter",
+  status: MoveCallStatus.BEGIN,
+  packageId: "",
+  modules: [],
+  currentModule: "",
+  functions: {},
+  currentFunction: "",
+  args: [],
+  argsUserInput: [],
+  error: "",
+  response: "",
 };
 
 const reducer = (state: MoveCallState, action: ActionType): MoveCallState => {
-    const { type, payload } = action;
-    switch (type) {
-        case MoveCallActionType.SET_MNEMONICS:
-            return {
-                ...state,
-                mnemonics: payload,
-            };
-        case MoveCallActionType.SET_PACKAGE_ID:
-            return {
-                ...state,
-                packageId: payload,
-            };
-        case MoveCallActionType.SET_MODULES:
-            return {
-                ...state,
-                modules: payload,
-            };
+  const { type, payload } = action;
+  switch (type) {
+    case MoveCallActionType.SET_MNEMONICS:
+      return {
+        ...state,
+        mnemonics: payload,
+      };
+    case MoveCallActionType.SET_PACKAGE_ID:
+      return {
+        ...state,
+        packageId: payload,
+      };
+    case MoveCallActionType.SET_MODULES:
+      return {
+        ...state,
+        modules: payload,
+      };
 
-        case MoveCallActionType.SET_CURRENT_MODULE:
-            return {
-                ...state,
-                currentModule: payload,
-            };
+    case MoveCallActionType.SET_CURRENT_MODULE:
+      return {
+        ...state,
+        currentModule: payload,
+      };
 
-        case MoveCallActionType.SET_FUNCTIONS:
-            return {
-                ...state,
-                functions: payload,
-            };
+    case MoveCallActionType.SET_FUNCTIONS:
+      return {
+        ...state,
+        functions: payload,
+      };
 
-        case MoveCallActionType.SET_ERROR:
-            return {
-                ...state,
-                status: MoveCallStatus.ERROR,
-                error: payload,
-            };
+    case MoveCallActionType.SET_ERROR:
+      return {
+        ...state,
+        status: MoveCallStatus.ERROR,
+        error: payload,
+      };
 
-        case MoveCallActionType.SET_CURRENT_FUNCTION:
-            return {
-                ...state,
-                currentFunction: payload,
-            };
+    case MoveCallActionType.SET_CURRENT_FUNCTION:
+      return {
+        ...state,
+        currentFunction: payload,
+      };
 
-        case MoveCallActionType.RESET_ARGS:
-            return {
-                ...state,
-                args: [],
-            };
+    case MoveCallActionType.RESET_ARGS:
+      return {
+        ...state,
+        args: [],
+      };
 
-        case MoveCallActionType.RESET_ARGS_USER_INPUT:
-            return {
-                ...state,
-                argsUserInput: [],
-            };
+    case MoveCallActionType.RESET_ARGS_USER_INPUT:
+      return {
+        ...state,
+        argsUserInput: [],
+      };
 
-        case MoveCallActionType.ADD_ARG:
-            return {
-                ...state,
-                args: [...state.args, payload],
-            };
+    case MoveCallActionType.ADD_ARG:
+      return {
+        ...state,
+        args: [...state.args, payload],
+      };
 
-        case MoveCallActionType.SET_VALUE_TO_ARG:
-            const argsUserInput = [...state.argsUserInput];
-            argsUserInput[payload.index] = payload.value;
-            return {
-                ...state,
-                argsUserInput,
-            };
+    case MoveCallActionType.SET_VALUE_TO_ARG:
+      const argsUserInput = [...state.argsUserInput];
+      argsUserInput[payload.index] = payload.value;
+      return {
+        ...state,
+        argsUserInput,
+      };
 
-        case MoveCallActionType.SET_RESPONSE:
-            return {
-                ...state,
-                status: MoveCallStatus.FINISH,
-                response: payload,
-            };
+    case MoveCallActionType.SET_RESPONSE:
+      return {
+        ...state,
+        status: MoveCallStatus.FINISH,
+        response: payload,
+      };
 
-        default:
-            throw new Error(`Unhandled action type: ${type}`);
-    }
+    default:
+      throw new Error(`Unhandled action type: ${type}`);
+  }
 };
 
 const sendMessage = (action: string, payload: any) => {
-    messageHandler.send(action, payload); // action, payload like redux
-  };
+  messageHandler.send(action, payload); // action, payload like redux
+};
 
 export interface IAppProps { }
 
 export const App: React.FunctionComponent<IAppProps> = ({ }: React.PropsWithChildren<IAppProps>) => {
-    const suiClient = useSuiClient();
-    const { network, selectNetwork } = useSuiClientContext();
+  const suiClient = useSuiClient();
+  const { network, selectNetwork } = useSuiClientContext();
 
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const [buildPath, setBuildPath] = useState<string>("");
-    const [publishPath, setPublishPath] = useState<string>("");
-    const [suiPath, setSuiPath] = useState<string>("");
+  const [buildPath, setBuildPath] = useState<string>("");
+  const [publishPath, setPublishPath] = useState<string>("");
+  const [suiPath, setSuiPath] = useState<string>("");
 
-    const handleNetworkChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        selectNetwork(e.target.value);
-    };
+  const handleNetworkChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    selectNetwork(e.target.value);
+  };
 
-    return (
-        <>
-            <SuiConfigProvider>
-                <MySuiAccountProvider>
-                    <h1 className="text-red-500">Sui Simulator</h1>
+  return (
+    <>
+      <SuiConfigProvider>
+        <MySuiAccountProvider>
+          <h1 className="text-red-500">Sui Simulator</h1>
                     <hr />
                     <SuiConfig/>
                     <hr />
                     <GasAddress />
                     <hr />
                     <BuildTestPublish />
-                    {/* <h2>Build</h2>
-                    <Input placeholder="Package Path" value={buildPath} onChange={(e) => setBuildPath(e.target.value)} />
-                    <Button onClick={() => { sendMessage("BUILD", { packagePath: buildPath, suiPath }); }}>
-                        Build
-                    </Button>
-                    <h2>Publish</h2>
-                    <Input placeholder="Package Path" value={publishPath} onChange={(e) => setPublishPath(e.target.value)} />
-                    <Button onClick={() => { sendMessage("PUBLISH", { packagePath: publishPath, suiPath }); }}>
-                        Publish
-                    </Button> */}
-                    <hr />
+          <hr />
 
                     <MoveCall state={state} dispatch={dispatch} />
 
                     <hr />
                     <Aliases />
-                </MySuiAccountProvider>
-            </SuiConfigProvider>
-        </>
-    );
+
+          
+        </MySuiAccountProvider>
+      </SuiConfigProvider>
+    </>
+  );
 };
